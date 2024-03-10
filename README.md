b97caa94ea8fc1d11ec6bf5892d73bcfc34b64e6
Maksym Panasenko <redussof@gmail.com>
2024-03-10 13:41:45 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Project BBR
+
+To run:
+- Build the solution
+- Place bbr\raylib-5.0_win64_msvc16\lib\raylib.dll into the output directory (bbr\project_bbr\bin\Debug\net8.0)
+- Choose x64 configuration
---
+++ project_bbr/Program.cs
@@ -0,0 +1,20 @@
+﻿using Raylib_cs;
+
+class Program
+{
+    public static void Main()
+    {
+        Raylib.InitWindow(800, 480, "Hello BBR");
+
+        while (!Raylib.WindowShouldClose())
+        {
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.White);
+
+            Raylib.DrawText("Hello, BBR!", 12, 12, 20, Color.Black);
+
+            Raylib.EndDrawing();
+        }
+        Raylib.CloseWindow();
+    }
+}
